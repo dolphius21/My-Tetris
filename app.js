3c810be79a1ad1fd4edff100c8e054729c8f7a2c
dolphius21 <johnrudolphlee@gmail.com>
2020-09-22 17:56:48 +0800

    building tetrominoes

--- app.js
+++ app.js
@@ -1,1 +1,85 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  // get the grid
+  const grid = document.querySelector(".grid");
+
+  const drawGrid = () => {
+    let divs = "";
+    for (let i = 0; i < 200; i++) {
+      divs += `<div></div>`;
+    }
+    grid.innerHTML = divs;
+  };
+
+  drawGrid();
+
+  // get the squares on the grid
+  let squares = Array.from(document.querySelectorAll(".grid div"));
+  const scoreDisplay = document.querySelector("#score");
+  const startBtn = document.querySelector("#start-button");
+  const width = 10;
+
+  // The Tetrominoes
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2, width * 2 + 1],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  ];
+
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+  ];
+
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1],
+  ];
+
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+  ];
+
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+  ];
+
+  const theTetrominoes = [
+    lTetromino,
+    zTetromino,
+    tTetromino,
+    oTetromino,
+    iTetromino,
+  ];
+
+  let currentPosition = 3;
+  let defaultRotation = 0;
+
+  // randomly select a Tetromino
+  let random = Math.floor(Math.random() * theTetrominoes.length);
+  let current = theTetrominoes[random][defaultRotation];
+
+  // draw the Tetromino
+  const draw = () => {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.add("tetromino");
+    });
+  };
+
+  // undraw the Tetromino
+  const undraw = () => {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.remove("tetromino");
+    });
+  };
+});
